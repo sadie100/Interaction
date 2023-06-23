da97d4b0fdcc93b08d8f52ae2ff95d5ae7e16362
김세희 <82367039+sadie100@users.noreply.github.com>
2023-06-23 16:54:30 +0900

    fix : 폰트 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import { Noto_Sans_KR } from "@next/font/google";
+
+const sans = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["100", "400", "500", "700", "900"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <main className={sans.className}>
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+    </main>
   );
 }
 
