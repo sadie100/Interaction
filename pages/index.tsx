94ef380b2ee87c2bca0623f300b5312ec42f6245
김세희 <sadie1001@naver.com>
2023-11-27 23:50:25 +0900

    chore: css 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div>
-      <p style={{ fontSize: "20px" }}>안녕하세요.</p>
-      <p style={{ fontSize: "20px" }}>
-        이곳은 제가 만들고 싶은 게임들과 효과들을 만드는 공간입니다.
-      </p>
+      <p style={{ fontSize: "20px" }}>홈 화면</p>
     </div>
   );
 };
